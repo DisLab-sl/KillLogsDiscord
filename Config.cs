5a96577f0645b842439dc81250bca9bd086a8424
dzarenafixers <dzarenafixersinfo@gmail.com>
2025-06-26 13:37:32 +0100

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,21 @@
+// -----------------------------------------------------------------------
+// <copyright file="Config.cs" company="YourName">
+// Copyright (c) YourName. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// SPDX-License-License-Identifier: CC-BY-SA-3.0
+
+using Exiled.API.Interfaces;
+
+namespace KillLogsDiscordBot
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public string DiscordBotToken { get; set; } = "Token-Here";
+        public ulong DiscordChannelId { get; set; } = Id channel here;
+        public bool LogScpKills { get; set; } = false;
+        public bool LogSuicides { get; set; } = true;
+    }
+}
